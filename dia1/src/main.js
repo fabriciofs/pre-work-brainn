d534ca864c6468e65c9a0732b6cf24decfd68530
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2021-08-17 09:54:13 -0300

    Exercise 4 - add hide / show div

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,26 @@
 import "./style.css";
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = document.querySelector('[data-js="app"]');
+
+const textByStatus = {
+  none: "Mostrar texto",
+  "": "Escoder texto acima",
+};
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+const link = document.querySelector('[data-js="link"]');
+link.innerHTML = textByStatus[app.style.display];
+
+link.addEventListener(
+  "click",
+  (event) => {
+    event.preventDefault();
+    app.style.display = app.style.display === "none" ? "" : "none";
+    link.innerHTML = textByStatus[app.style.display];
+  },
+  false
+);
