164c46a31324a666f0f9fccae4814cb52c35b651
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2021-08-19 12:00:34 -0300

    Exercise 2 - Box Color

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -22,3 +22,39 @@ const capitalizeText = (text) =>
 
 const isLowerCaseWord = (word) =>
   (word = ["de", "da", "do", "dos"].includes(word));
+
+const form = document.querySelector('[data-js="form"]');
+const selectColors = document.createElement("select");
+selectColors.setAttribute("multiple", true);
+
+form.appendChild(selectColors);
+
+const colors = [
+  { color: "#ff0000", label: "Red" },
+  { color: "#00ff00", label: "Green" },
+  { color: "#0000ff", label: "Blue" },
+  { color: "#ffff00", label: "Yellow" },
+  { color: "#ff8000", label: "Orange" },
+];
+
+colors.forEach((color) => {
+  selectColors.add(new Option(color.label, color.color));
+});
+
+const divResult = document.createElement("div");
+divResult.classList.add("div-result");
+form.appendChild(divResult);
+
+selectColors.addEventListener("change", (e) => {
+  e.preventDefault();
+  divResult.innerHTML = "";
+  [...e.target.options]
+    .filter((el) => el.selected)
+    .forEach((el) => {
+      const div = document.createElement("div");
+      div.style.background = el.value;
+      div.style.width = "100px";
+      div.style.height = "100px";
+      divResult.appendChild(div);
+    });
+});
